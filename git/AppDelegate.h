8b5c981f444a9e1afd6b9e31b7a744b8861aab20
wqx <wqx@macbook-pro.local>
2017-02-20 11:52:50 +0800

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by wqx on 2017/2/20.
+//  Copyright © 2017年 yueling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by wqx on 2017/2/20.
+//  Copyright © 2017年 yueling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
